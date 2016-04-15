2ffb1bad55859f8ead58890b14b5bd33eedfebe4
Taylor Burton <burton.taylor@gmail.com>
2016-04-14 20:42:40 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,10 @@
 import webbrowser
 
+"""webbrowser is imported at the top of the program so that it can be called
+later in the program to open a web browser"""
+
+"""The class Movie is created, which will create a movie object, containing
+the movie's title, story line, poster image and youtube trailer"""
 
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
@@ -8,6 +13,9 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+"""The show_trailer function is created below to open a web browser
+which will display a youtube trailer when a Movie object is called"""
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
